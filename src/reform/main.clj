3c7991690dc1f8e64037b8f8b5ad054fce16678f
Gareth Jones <gjones@drwholdings.com>
2012-09-21 15:52:32 -0500

    cookies

--- src/reform/main.clj
+++ src/reform/main.clj
@@ -2,12 +2,24 @@
   (:use [compojure.handler :only [api]]
         [compojure.core :only [GET POST defroutes]]
         [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.cookies :only [wrap-cookies]]
         [ring.util.response :only [response content-type]])
   (:require [cheshire.core :as json]))
 
 (def treatments
-  {:color "red"
-   :text  "Sign Up For Free!"})
+  [{:color "red"
+    :text  "Sign Up For Free!"}
+   {:color "blue"
+    :text  "Sign Up!"}
+   {:color "yellow"
+    :text "DONT SIGN UP"}])
+
+(let [next (atom (cycle (range (count treatments))))]
+  (defn next-treatment
+    []
+    (let [res (first @next)]
+      (swap! next rest)
+      res)))
 
 (defn jsonp-response
   [body callback]
@@ -18,15 +30,23 @@
 
 (defn wrap-request-logging
   [handler]
-  (fn [{:keys [uri query-string] :as req}]
-    (locking System/out (println "request" uri query-string))
+  (fn [{:keys [uri query-string cookies] :as req}]
+    (locking System/out (println "request:" uri query-string "cookies:" cookies))
     (handler req)))
 
+(defn get-treatment
+  [{cookies :cookies {callback :callback} :params}]
+  (if-let [{value :value} (cookies "treatment")]
+    (jsonp-response (nth treatments (Integer. value)) callback)
+    (-> (jsonp-response (nth treatments (next-treatment)) callback)
+        (assoc-in [:cookies :treatment] treatment))))
+
 (defroutes api-routes
-  (GET "/" [callback] (jsonp-response treatments callback)))
+  (GET "/" req (get-treatment req)))
 
 (def app (-> #'api-routes
              wrap-request-logging
+             wrap-cookies
              api))
 
 (defn -main
