93a21e7d7ce77bebb211871135c956452378f126
Gareth Jones <gjones@drwholdings.com>
2012-09-21 14:27:23 -0500

    json response

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url ""
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
-                 [compojure "1.1.3"]]
+                 [compojure "1.1.3"]
+                 [cheshire "4.0.2"]]
   :min-lein-version "2.0.0")
--- src/reform/main.clj
+++ src/reform/main.clj
@@ -1,10 +1,22 @@
 (ns reform.main
   (:use [compojure.handler :only [api]]
         [compojure.core :only [GET POST defroutes]]
-        [ring.adapter.jetty :only [run-jetty]]))
+        [ring.adapter.jetty :only [run-jetty]]
+        [ring.util.response :only [response content-type]])
+  (:require [cheshire.core :as json]))
+
+(def treatments
+  {})
+
+(defn json-response
+  [body]
+  (-> body
+      json/encode
+      response
+      (content-type "application-json")))
 
 (defroutes api-routes
-  (GET "/" [] "HI!"))
+  (GET "/" [] (json-response treatments)))
 
 (def app (-> #'api-routes
              api))
